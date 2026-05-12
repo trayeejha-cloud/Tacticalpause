673ae3e78c65dd65bf9f9a5988412f3cb2dc8d90
trayeejha-cloud <trayeejha@gmail.com>
2026-05-12 19:05:40 -0400

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  
+  base: "/tacticalpause/",
+
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
